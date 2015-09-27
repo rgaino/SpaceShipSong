27877aeb7662077a4ed72394e9a2e677911ecd08
Rafael Gaino <rgaino@gmail.com>
2015-09-27 16:32:28 -0300

    Now showing all 6 tracks with a 3d visualization

--- js/audio.js
+++ js/audio.js
@@ -1,22 +1,30 @@
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
 var context = new AudioContext();
 var tickTimer;
 
+var tracks = [
+              'audio/drums.mp3',
+              'audio/bass.mp3',
+              'audio/guitar_chords.mp3',
+              'audio/guitar_lead.mp3',
+              'audio/guitar_solo.mp3',
+              'audio/percussion.mp3'
+            ];
+
+var trackCount = tracks.length;
 var audioSources = [trackCount];
 var analyserNodes = [trackCount];
 var freqFloatData  = [trackCount];
 var freqByteData = [trackCount];
 var timeByteData  = [trackCount];
 var loadedCount = 0;
-var trackCount = 4;
 
-// loadSound('audio/kick.mp3', 0);
-loadSound('audio/drums.mp3', 1);
-loadSound('audio/bass.mp3', 3);
-loadSound('audio/guitar_chords.mp3', 2);
-loadSound('audio/guitar_lead.mp3', 0);
 
+for(var trackNumber=0; trackNumber<trackCount; trackNumber++) {
+  loadSound(tracks[trackNumber], trackNumber);
+}
 
 function loadSound(url, trackNumber) {
   var request = new XMLHttpRequest();
@@ -26,28 +34,26 @@ function loadSound(url, trackNumber) {
   // Decode asynchronously
   request.onload = function() {
     context.decodeAudioData(request.response, function(buffer) {
+  		loadedCount++;
+  		console.log("loaded track number " + trackNumber + ", loaded count: " + loadedCount);
 
-		loadedCount++;
-		console.log("loaded track number " + trackNumber + ", loaded count: " + loadedCount);
+  		audioSources[trackNumber] = context.createBufferSource();
+  		audioSources[trackNumber].buffer = buffer;
 
-		audioSources[trackNumber] = context.createBufferSource();
-		audioSources[trackNumber].buffer = buffer;
+  		analyserNodes[trackNumber] = context.createAnalyser();
+  		analyserNodes[trackNumber].fftSize = 32;
+  		analyserNodes[trackNumber].smoothingTimeConstant = 0.85;
 
-		analyserNodes[trackNumber] = context.createAnalyser();
-		analyserNodes[trackNumber].fftSize = 32;
-		analyserNodes[trackNumber].smoothingTimeConstant = 0.85;
+  		audioSources[trackNumber].connect(analyserNodes[trackNumber]);
+  		analyserNodes[trackNumber].connect(context.destination);
 
-		audioSources[trackNumber].connect(analyserNodes[trackNumber]);
-		analyserNodes[trackNumber].connect(context.destination);
+  		freqFloatData[trackNumber] = new Float32Array(analyserNodes[trackNumber].frequencyBinCount);
+  		freqByteData[trackNumber]  = new Uint8Array(analyserNodes[trackNumber].frequencyBinCount);
+  		timeByteData[trackNumber]  = new Uint8Array(analyserNodes[trackNumber].frequencyBinCount);
 
-		freqFloatData[trackNumber] = new Float32Array(analyserNodes[trackNumber].frequencyBinCount);
-		freqByteData[trackNumber]  = new Uint8Array(analyserNodes[trackNumber].frequencyBinCount);
-		timeByteData[trackNumber]  = new Uint8Array(analyserNodes[trackNumber].frequencyBinCount);
-
-    if(loadedCount == trackCount) {
-			document.getElementById("playButton").disabled = false;
-		}
-
+      if(loadedCount == trackCount) {
+  			document.getElementById("playButton").disabled = false;
+  		}
     });
   }
   request.send();
@@ -86,23 +92,7 @@ function getAverage(array) {
 }
 
 function log() {
-
   console.log("Log at ", new Date());
-
-  console.log(cubeArray);
-  // console.log(freqByteData[0].length);
-  // console.log( (getAverage(freqByteData[0]) / 100) + 1);
-  // console.log( getAverage(freqByteData[0]));
-
-  for(var i=0; i<trackCount; i++) {
-    // console.log(freqFloatData[i][0]);
-    // console.log(freqByteData[i][0]);
-    // console.log(timeByteData[i][0]);
-
-    // console.log("Data for track " + i);
-    // console.log("dBs: " + getAverage(freqFloatData[i]));
-    // console.log("Frequency: " + getAverage(freqByteData[i]));
-    // console.log("Waveform: " + getAverage(timeByteData[i]));
-
-    }
+  console.log( freqByteData );
+  console.log( scene );
 }
--- js/visual.js
+++ js/visual.js
@@ -2,45 +2,68 @@
 var cubeArray = [trackCount];
 
 function createScene() {
+
+  //setup Threejs scene, camera and renderer
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var xPosition = -10;
-  var xPositionOffset = 0.5;
+  //each view group contains 16 bars that shows the track frequency information
+  var trackViewGroups = [trackCount];
+  var trackViewColor = [0x00FF00,
+                        0xFF66FF,
+                        0xFF9900,
+                        0x0066FF,
+                        0xFFFF00,
+                        0xFFFFFF];
+  var groupY = -20;
 
-  for(var i=0; i<=freqByteData[0].length; i++) {
-  //
-  //   console.log("Creating cube number " + i + " at x=" + xPosition);
+  for(var trackNumber=0; trackNumber<trackCount; trackNumber++) {
 
-    var cube = new THREE.Mesh( geometry, material );
-    cube.position.x = xPosition;
-    xPosition += (xPositionOffset + cube.scale.x);
-    cubeArray[i] = cube;
+    console.log("Creating track view group " + trackNumber);
 
-    scene.add(cube);
+    group = new THREE.Object3D();
+    group.position = new THREE.Vector3(0, groupY, 0);
+    group.name = trackNumber;
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: trackViewColor[trackNumber] } );
+    var xPosition = -10;
+    var xPositionOffset = 0.5;
+
+    for(var i=0; i<=freqByteData[trackNumber].length; i++) {
+      var cube = new THREE.Mesh( geometry, material );
+      cube.position.y = groupY;
+      cube.position.x = xPosition;
+      xPosition += (xPositionOffset + cube.scale.x);
+      cube.name = trackNumber + '.' + i;
+      cubeArray[i] = cube;
+
+      group.add(cube);
+    }
+
+    trackViewGroups[trackNumber] = group;
+    scene.add(group);
+    groupY += 10;
   }
-  console.log("Total scene.children=" + scene.children.length);
 
-  camera.position.z = 5;
+  camera.position.z = 50;
+  controls = new THREE.OrbitControls(camera, document, renderer.domElement);
 
   var render = function () {
+
     requestAnimationFrame( render );
 
-    for(var i=0; i<=freqByteData[0].length; i++) {
-      console.log(freqByteData[i]);
-      cubeArray[i].scale.y = (freqByteData[0][i] / 100) + 1;
+    for(var trackNumber=0; trackNumber<trackCount; trackNumber++) {
+      for(var i=0; i<=freqByteData[trackNumber].length; i++) {
+        // look for the current bar for the current track group
+        //they are named 0.0, 0.1, 0.2... 1.0, 1.1, 1.2,... etc.
+        var cube = scene.getObjectByName( trackNumber + '.' + i, true );
+        cube.scale.y = (freqByteData[trackNumber][i] / 100) + 1;
+      }
     }
-    // cube.rotation.x += 0.1;
-    // cube.rotation.y += 0.1;
-    // cube.scale.y = (getAverage(freqByteData[0]) / 100) + 1;
-    // console.log(cube.scale.y);
-
 
     renderer.render(scene, camera);
   };
