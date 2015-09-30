b18cac69671c1a3b1decec249d52f5b640e8ee41
Rafael Gaino <rgaino@gmail.com>
2015-09-30 13:43:51 -0300

    Removed unused variables and fixed scaling error

--- js/visual.js
+++ js/visual.js
@@ -1,6 +1,4 @@
 
-var cubeArray = [trackCount];
-
 function createScene() {
 
   //setup Threejs scene, camera and renderer
@@ -11,25 +9,14 @@ function createScene() {
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  //each view group contains 16 bars that shows the track frequency information
-  var trackViewGroups = [trackCount];
-  var trackViewColor = [0x00FF00,
-                        0xFF66FF,
-                        0xFF9900,
-                        0x0066FF,
-                        0x000033,
-                        0xB82E00];
-  var trackViewNames = ['drums', 'bass', 'base guitar', 'lead guitar', 'solo guitar', 'percussion'];
+  var trackViewColor = [0x00FF00, 0xFF66FF, 0xFF9900,       0x0066FF,     0x000033,      0xB82E00];
+  var trackViewNames = ['drums',  'bass',   'base guitar', 'lead guitar', 'solo guitar', 'percussion'];
 
   var groupY = -25;
 
   for(var trackNumber=0; trackNumber<trackCount; trackNumber++) {
 
     console.log("Creating track view group " + trackNumber);
-
-    group = new THREE.Object3D();
-    group.position = new THREE.Vector3(0, groupY, 0);
-    group.name = trackNumber;
 
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshLambertMaterial( { color: trackViewColor[trackNumber] } );
@@ -42,31 +29,20 @@ function createScene() {
       cube.position.x = xPosition;
       xPosition += (xPositionOffset + cube.scale.x);
       cube.name = trackNumber + '.' + i;
-      cubeArray[i] = cube;
-
-      group.add(cube);
+      scene.add(cube);
     }
 
     xPosition += (xPositionOffset + cube.scale.x);
 
-    var textParams = {
-            					size: 1.2,
-            					height: 1,
-            					curveSegments: 2,
-            					font: "helvetiker"
-            				};
-
     //track name in 3D text, using same material as frequency bars
+    var textParams = { size: 1.2, height: 1, curveSegments: 2, font: "helvetiker" };
+    var textMaterial = new THREE.MeshLambertMaterial( { color: trackViewColor[trackNumber] } );
     var trackNameGeometry = new THREE.TextGeometry(trackViewNames[trackNumber].toUpperCase(), textParams);
-    var text3D = new THREE.Mesh( trackNameGeometry, material );
+    var text3D = new THREE.Mesh( trackNameGeometry, textMaterial );
     text3D.position.x = xPosition;
     text3D.position.y = groupY;
+    scene.add(text3D);
 
-    group.add(text3D);
-
-    trackViewGroups[trackNumber] = group;
-
-    scene.add(group);
     groupY += 10;
   }
 
@@ -87,7 +63,7 @@ function createScene() {
         // look for the current bar for the current track group
         //they are named 0.0, 0.1, 0.2... 1.0, 1.1, 1.2,... etc.
         var cube = scene.getObjectByName( trackNumber + '.' + i, true );
-        cube.scale.y = (freqByteData[trackNumber][i] / 100) ;
+        cube.scale.y = (freqByteData[trackNumber][i] / 100) + 0.1 ;
       }
     }
 
